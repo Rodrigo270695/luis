5242251a321e2e722daa79544316080104e0cfd2
Rodrigo270695 <pc@DESKTOP-U6Q10CU>
2022-10-06 16:07:30 -0500

    Luis

--- src/cadaData/Conexion.java
+++ src/cadaData/Conexion.java
@@ -1,6 +1,28 @@
 
 package cadaData;
 
+import java.sql.*;
+
 public class Conexion {
+    
+    private final String URL ="jdbc:mysql://localhost:3308/bd_caritas";
+    private final String USUARIO ="root";
+    private final String PASSWORD ="admin";
+    
+    public Connection conectar(){
+        
+        Connection con = null;
+        
+        try {
+            
+            con = DriverManager.getConnection(URL, USUARIO, PASSWORD);
+            System.out.println("Conexión Exitosa");
+        } catch (SQLException e) {
+            System.out.println("Error al conectar");
+            e.printStackTrace(System.err);
+        }
+        
+        return con;
+    }
     
 }
